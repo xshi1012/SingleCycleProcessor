320b59dceb28a689071d112f2235ded0805944ea
Xiaotian Shi <xshi@UCSD.EDU>
2019-11-25 20:24:31 -0800

    program1 finished

--- basic_processor/TopLevel_tb.sv
+++ basic_processor/TopLevel_tb.sv
@@ -10,22 +10,24 @@ module TopLevel_tb;	     // Lab 17
 
 // program 1-specific variables
 logic[11:1] d1_in[15];       // original messages
-logic       p8, p4, p2, p1;  // Hamming block parity bits
+logic       p16, p8, p4, p2, p1;  // Hamming block parity bits
 logic[15:1] d1_out[15];      //	orig messages w/ parity inserted
 
 // program 2-specific variables
 logic[11:1] d2_in[15];           // use to generate ddata
 logic[15:1] d2_good[15];         // d2_in w/ parity
 logic[ 3:0] flip[15];        // position of corruption bit
+logic[ 5:0] flip2[15];
+logic[15:0] d2_bad1[15];
 logic[15:1] d2_bad[15];      // possibly corrupt messages w/ parity
-logic       s8, s4, s2, s1;  // parity generated from data of d_bad
+logic       s16, s8, s4, s2, s1;  // parity generated from data of d_bad
 logic[ 3:0] err;             // bitwise XOR of p* and s* as 4-bit vector        
 logic[11:1] d2_corr[15];     // recovered and corrected messages
 
 // program 3-specific variables
 logic[7:0] ctb,		       // how many bytes hold the pattern?
            cts,		       // how many pattern in the whole string?
-		   cto;		       // how many pattern fit inside each byte?
+	   cto;		       // how many pattern fit inside each byte?
 logic      ctp;		       // flags occurrence of patern in a given byte
 logic[4:0] pat;            // pattern to search for
 logic[255:0] str2; 	       // message string
@@ -48,24 +50,11 @@ logic[  7:0] mat_str[32];  // message string parsed into bytes
 initial begin
 // program 1
   for(int i=0;i<15;i++)	begin
-    d1_in[i] = $random;              // create 15 messages
+    d1_in[i] = $random;        // create 15 messages
 // copy 15 original messages into first 30 bytes of memory 
-// rename "dm1" and/or "core" if you used different names for these
-    DUT.data_memory.core[2*i+1]  = {5'b0,d1_in[i][11:9]};	 // concatenate
-    DUT.data_memory.core[2*i]    = d1_in[i][8:1];
-  end
-
-  for(int i=0; i<15; i++) begin
-	d2_in[i] = $random;
-    p8 = ^d2_in[i][11:5];
-    p4 = (^d2_in[i][11:8])^(^d2_in[i][4:2]); 
-    p2 = d2_in[i][11]^d2_in[i][10]^d2_in[i][7]^d2_in[i][6]^d2_in[i][4]^d2_in[i][3]^d2_in[i][1];
-    p1 = d2_in[i][11]^d2_in[i][ 9]^d2_in[i][7]^d2_in[i][5]^d2_in[i][4]^d2_in[i][2]^d2_in[i][1];
-    d2_good[i] = {d2_in[i][11:5],p8,d2_in[i][4:2],p4,d2_in[i][1],p2,p1};
-    flip[i] = $random;
-    d2_bad[i] = d2_good[i] ^ (1'b1<<flip[i]);
-	DUT.data_memory.core[65+2*i] = {1'b0,d2_bad[i][15:9]};
-    DUT.data_memory.core[64+2*i] = {d2_bad[i][8:1]};
+// rename "data_memory" and/or "core" if you used different names for these
+    DUT.data_memory.core[2*i+1]  = {5'b0,d1_in[i][11:9]};
+    DUT.data_memory.core[2*i]    =       d1_in[i][ 8:1];
   end
 
   // program 3
@@ -99,35 +88,29 @@ initial begin
   #10ns start = 0;
 
   wait(halt);
+
+
   // generate parity for each message; display result and that of DUT
-  /*$display("start program 1");
+  $display("start program 1");
   $display();
   for(int i=0;i<15;i++) begin
-    p8 = ^d1_in[i][11:5];  // reduction XOR (parity) operation
+    p8 = ^d1_in[i][11:5];
     p4 = (^d1_in[i][11:8])^(^d1_in[i][4:2]); 
     p2 = d1_in[i][11]^d1_in[i][10]^d1_in[i][7]^d1_in[i][6]^d1_in[i][4]^d1_in[i][3]^d1_in[i][1];
     p1 = d1_in[i][11]^d1_in[i][ 9]^d1_in[i][7]^d1_in[i][5]^d1_in[i][4]^d1_in[i][2]^d1_in[i][1];
-// assemble output (data with parity embedded)
-    $displayb ({1'b0,d1_in[i][11:5],p8,d1_in[i][4:2],p4,d1_in[i][1],p2,p1});
-    $writeb  (DUT.dm1.core[31+2*i]);
-    $displayb(DUT.dm1.core[30+2*i]);
-	if({1'b0,d1_in[i][11:5],p8,d1_in[i][4:2],p4,d1_in[i][1],p2,p1}!=
-       {DUT.dm1.core[31+2*i],DUT.dm1.core[30+2*i]}) $display("****Oh S***!");
+    p16 = ^d1_in[i]^p8^p4^p2^p1;  // overall parity (16th bit)
+    // assemble output (data with parity embedded)
+    $displayb ({d1_in[i][11:5],p8,d1_in[i][4:2],p4,d1_in[i][1],p2,p1,p16});
+    $writeb  (DUT.data_memory.core[31+2*i]);
+    $displayb(DUT.data_memory.core[30+2*i]);
+    if ({d1_in[i][11:5],p8,d1_in[i][4:2],p4,d1_in[i][1],p2,p1,p16} != {DUT.data_memory.core[31+2*i],DUT.data_memory.core[30+2*i]}) begin
+        $display("?");    
+    end
     $display();
-   end
+  end
 
-  $display();
-  $display("start program 2");
-  $display();
-  for(int i=0; i<15; i++) begin
-    $displayb({5'b0,d2_in[i]});
-    $writeb  (DUT.dm1.core[95+2*i]);
-    $displayb(DUT.dm1.core[94+2*i]);  
-	if({5'b0,d2_in[i]}!={DUT.dm1.core[95+2*i],DUT.dm1.core[94+2*i]})
-	  $display("****Not Again!****");
-	$display();
-  end*/
 
+/* Program3 check
   $display();
   $display("start program 3");
   $display();
@@ -137,7 +120,7 @@ initial begin
   if(cto!=DUT.data_memory.core[193]) $display("**** oops!****");
   $display("cts = %d %d",cts,DUT.data_memory.core[194]);
   if(cts!=DUT.data_memory.core[194]) $display("**** oops!****");
-
+*/
   #30ns $stop;			   
 end
 
